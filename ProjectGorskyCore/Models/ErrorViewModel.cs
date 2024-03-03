04ff691e533994a3a1cf40fc4f0af713c486cc90
llzai <llzaicev@yandex.ru>
2024-03-03 23:43:50 +0300

    Добавьте файлы проекта.

---
+++ ProjectGorskyCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectGorskyCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
